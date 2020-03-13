262a2005e0a8cef592713cb8b3775dfd91c799ec
kevinpatel0801 <54414024+kevinpatel0801@users.noreply.github.com>
2020-03-13 10:35:02 -0400

    Add files via upload

---
+++ Prime/primepackage/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+"""
+This is importing all the modules
+"""
+
+
+__author__ = "Kevin Patel"
+__license__ = "GPL"
+__version__ = "0.1"
+__email__ = "kevpatel@vsu"
+__status__ = "done"
+
+
+from .primeio import write_primes
+from .primeio import read_primes
+from .primemodule import getNPrime
